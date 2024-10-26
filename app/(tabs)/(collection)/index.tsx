ebbc54890489aea0d7a61c8dbf55a1c9cd86b890
Ha Quang <quangha782002@gmail.com>
2024-10-26 16:18:56 +0700

    update collection screen

--- app/(tabs)/(collection)/index.tsx
+++ app/(tabs)/(collection)/index.tsx
@@ -2,18 +2,18 @@ import { View, Text, ScrollView, TextInput, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FontAwesome } from "@expo/vector-icons";
 
-const categories = [
-  { id: 1, name: "Waterfalls", image: "https://example.com/waterfall.jpg" },
-  { id: 2, name: "Beaches", image: "https://example.com/beach.jpg" },
-  { id: 3, name: "Restaurants", image: "https://example.com/restaurant.jpg" },
+const collections = [
+  { id: 1, name: "Waterfalls", image: "https://kyluc.vn/userfiles/upload/images/modules/news/2016/7/11/0_hinh-anh-thien-nhien-dep-nhat-th-gioi.jpg" },
+  { id: 2, name: "Beaches", image: "https://www.vietnamworks.com/hrinsider/wp-content/uploads/2023/12/hinh-nen-thien-nhien-3d-001.jpg" },
+  { id: 3, name: "Restaurants", image: "https://posapp.vn/wp-content/uploads/2020/09/%C4%91%E1%BB%93ng-b%E1%BB%99-n%E1%BB%99i-th%E1%BA%A5t.jpg" },
 ];
 
 const CollectionScreen = () => {
   return (
     <SafeAreaView className="h-full bg-white">
       <ScrollView className="w-full px-4">
-        <View className="mt-4 flex items-center">
-          <Text className="text-2xl font-semibold">Collections</Text>
+        <View className="flex items-center pt-1">
+          <Text className="color-black flex-1 text-center font-InterBold text-xl">Collections</Text>
         </View>
 
         <View className="mt-4 flex flex-row items-center rounded-full border bg-gray-100 px-4 py-2">
@@ -25,14 +25,14 @@ const CollectionScreen = () => {
         </View>
 
         <View className="mt-6 flex flex-row justify-around">
-          {categories.map((category) => (
-            <View key={category.id} className="items-center">
+          {collections.map((collection) => (
+            <View key={collection.id} className="items-center">
               <Image
-                source={{ uri: category.image }}
+                source={{ uri: collection.image }}
                 className="h-24 w-24 rounded-lg"
                 resizeMode="cover"
               />
-              <Text className="mt-2 text-center text-sm">{category.name}</Text>
+              <Text className="mt-2 text-center text-sm">{collection.name}</Text>
             </View>
           ))}
         </View>
